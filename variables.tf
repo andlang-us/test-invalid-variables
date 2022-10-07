a5131a5d1c5ac5ba684a8c1aee2861a35180883d
andlang <andrew.l.lang@oracle.com>
2022-10-07 13:13:19 -0700

    test

--- variables.tf
+++ variables.tf
@@ -21,10 +21,6 @@
     variable "subnet_dns_label" {
       default = "subnet"
     }
-
-    variable "test" {
-      default = "dummy"
-    }
     
     provider "oci" {
       tenancy_ocid     = var.tenancy_ocid
