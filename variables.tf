7e75dde8ab20ace8956f6701314cc5aabfc04e78
andlang <andrew.l.lang@oracle.com>
2022-10-12 10:18:38 -0700

    test new variable

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 
-    // Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
-    // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-    
-    variable "compartment_ocid" {}
-    variable "tenancy_ocid" {}
-    variable "region" {}
+// Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-    variable "vcn_display_name" {
-      default = "testVCN"
-    }
-    
-    variable "vcn_cidr" {
-      default = "10.0.0.0/16"
-    }
+variable "compartment_ocid" {}
+variable "tenancy_ocid" {}
+variable "region" {}
 
-    variable "vcn_dns_label" {
-      default     = "vcn"
-    }
-    
-    provider "oci" {
-      tenancy_ocid     = var.tenancy_ocid
-      region           = var.region
-    }
+variable "vcn_display_name" {
+  default = "testVCN"
+}
+
+variable "vcn_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "vcn_dns_label" {
+  default = "vcn"
+}
+
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+}
